45753b341c4432d91a95a2921c8124292a05d03d
acoppes <ariel.coppes@gemserk.com>
2010-12-10 18:23:45 +0000

    added an enabled property to filemonitorresourcehelper to disable it.
    git-svn-id: http://jresourcesmanager.googlecode.com/svn/trunk@250 7ae9f935-f82b-96db-1286-7e115280eeba

--- resourcesmanager-core/src/main/java/com/gemserk/resources/monitor/FileMonitorResourceHelper.java
+++ resourcesmanager-core/src/main/java/com/gemserk/resources/monitor/FileMonitorResourceHelper.java
@@ -8,20 +8,28 @@ import com.google.inject.Inject;
 
 @SuppressWarnings("unchecked")
 public class FileMonitorResourceHelper {
-	
+
 	FilesMonitor filesMonitor;
-	
+
+	Boolean enabled = true;
+
 	@Inject
 	public void setFilesMonitor(FilesMonitor filesMonitor) {
 		this.filesMonitor = filesMonitor;
 	}
-	
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
 	public void monitorClassPathFile(String file, Resource resource) {
-		filesMonitor.monitor(FileUtils.classPathFile(file), new ReloadResourceWhenFileModified(resource));
+		if (enabled)
+			filesMonitor.monitor(FileUtils.classPathFile(file), new ReloadResourceWhenFileModified(resource));
 	}
 
 	public void monitorFileSystemFile(String file, Resource resource) {
-		filesMonitor.monitor(new File(file), new ReloadResourceWhenFileModified(resource));
+		if (enabled)
+			filesMonitor.monitor(new File(file), new ReloadResourceWhenFileModified(resource));
 	}
-	
+
 }
