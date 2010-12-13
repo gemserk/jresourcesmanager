10426f06b8b40b7fc63c430175a980b58988ef1b
acoppes <ariel.coppes@gemserk.com>
2010-12-13 17:11:30 +0000

    changed to use PropertiesLoader to load a Properties from a file.
    git-svn-id: http://jresourcesmanager.googlecode.com/svn/trunk@292 7ae9f935-f82b-96db-1286-7e115280eeba

--- resourcesmanager-slickmodule/src/main/java/com/gemserk/resources/slick/PropertiesAnimationLoader.java
+++ resourcesmanager-slickmodule/src/main/java/com/gemserk/resources/slick/PropertiesAnimationLoader.java
@@ -1,9 +1,8 @@
 package com.gemserk.resources.slick;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
+import com.gemserk.resources.PropertiesLoader;
 import com.gemserk.resources.dataloaders.DataLoader;
 import com.gemserk.resources.datasources.ClassPathDataSource;
 import com.gemserk.resources.resourceloaders.ResourceLoaderImpl;
@@ -12,37 +11,34 @@ import com.gemserk.resources.slick.dataloaders.SlickAnimationLoader;
 @SuppressWarnings("unchecked")
 public class PropertiesAnimationLoader extends PropertiesBaseLoader {
 
+	PropertiesLoader propertiesLoader = new PropertiesLoader();
+
 	public void load(String propertiesFile) {
-		try {
-			Properties properties = new Properties();
-			InputStream propertiesInputStream = new ClassPathDataSource(propertiesFile).getInputStream();
-			properties.load(propertiesInputStream);
 
-			for (Object keyObj : properties.keySet()) {
-				String id = (String) keyObj;
-				String value = properties.getProperty(id);
+		Properties properties = propertiesLoader.load(new ClassPathDataSource(propertiesFile));
 
-				String[] values = value.split(",");
-				String file = values[0];
-				int width = Integer.parseInt(values[1]);
-				int height = Integer.parseInt(values[2]);
-				final int time = Integer.parseInt(values[3]);
-				final int framesCount = Integer.parseInt(values[4]);
+		for (Object keyObj : properties.keySet()) {
+			String id = (String) keyObj;
+			String value = properties.getProperty(id);
 
-				DataLoader dataLoader = new SlickAnimationLoader(file, width, height, time, framesCount, false);
-				resourceManager.add(id, new ResourceLoaderImpl(dataLoader));
+			String[] values = value.split(",");
+			String file = values[0];
+			int width = Integer.parseInt(values[1]);
+			int height = Integer.parseInt(values[2]);
+			final int time = Integer.parseInt(values[3]);
+			final int framesCount = Integer.parseInt(values[4]);
 
-				// // mark the resource for reloading whenever the properties file was modified
-				// resourcesMonitor.monitor(new ResourceMonitor(resourceManager.get(id), FileMonitorFactory.classPathFileMonitor(propertiesFile)));
+			DataLoader dataLoader = new SlickAnimationLoader(file, width, height, time, framesCount, false);
+			resourceManager.add(id, new ResourceLoaderImpl(dataLoader));
 
-				// or the resource file itself
-				// resourcesMonitor.monitor(new ResourceMonitor(resourceManager.get(id), FileMonitorFactory.classPathFileMonitor(file)));
-				fileMonitorResourceHelper.monitorClassPathFile(file, resourceManager.get(id));
-			}
+			// // mark the resource for reloading whenever the properties file was modified
+			// resourcesMonitor.monitor(new ResourceMonitor(resourceManager.get(id), FileMonitorFactory.classPathFileMonitor(propertiesFile)));
 
-		} catch (IOException e) {
-			throw new RuntimeException("failed to load animations from " + propertiesFile, e);
+			// or the resource file itself
+			// resourcesMonitor.monitor(new ResourceMonitor(resourceManager.get(id), FileMonitorFactory.classPathFileMonitor(file)));
+			fileMonitorResourceHelper.monitorClassPathFile(file, resourceManager.get(id));
 		}
+
 	}
 
 }
--- resourcesmanager-slickmodule/src/main/java/com/gemserk/resources/slick/PropertiesSoundLoader.java
+++ resourcesmanager-slickmodule/src/main/java/com/gemserk/resources/slick/PropertiesSoundLoader.java
@@ -1,9 +1,8 @@
 package com.gemserk.resources.slick;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
+import com.gemserk.resources.PropertiesLoader;
 import com.gemserk.resources.dataloaders.DataLoader;
 import com.gemserk.resources.datasources.ClassPathDataSource;
 import com.gemserk.resources.resourceloaders.CachedResourceLoader;
@@ -13,30 +12,27 @@ import com.gemserk.resources.slick.dataloaders.SlickSoundLoader;
 @SuppressWarnings("unchecked")
 public class PropertiesSoundLoader extends PropertiesBaseLoader {
 
+	PropertiesLoader propertiesLoader = new PropertiesLoader();
+
 	public void load(String propertiesFile) {
-		try {
-			Properties properties = new Properties();
-			InputStream propertiesInputStream = new ClassPathDataSource(propertiesFile).getInputStream();
-			properties.load(propertiesInputStream);
 
-			for (String id : properties.stringPropertyNames()) {
-				String imageProperties = properties.getProperty(id);
-				String[] values = imageProperties.split(",");
+		Properties properties = propertiesLoader.load(new ClassPathDataSource(propertiesFile));
 
-				String file = values[0];
+		for (String id : properties.stringPropertyNames()) {
+			String imageProperties = properties.getProperty(id);
+			String[] values = imageProperties.split(",");
 
-				DataLoader dataLoader = new SlickSoundLoader(file);
-				resourceManager.add(id, new CachedResourceLoader(new ResourceLoaderImpl(dataLoader)));
+			String file = values[0];
 
-				// // mark the resource for reloading whenever the properties file was modified
-				// resourcesMonitor.monitor(new ResourceMonitor(resourceManager.get(id), FileMonitorFactory.classPathFileMonitor(propertiesFile)));
+			DataLoader dataLoader = new SlickSoundLoader(file);
+			resourceManager.add(id, new CachedResourceLoader(new ResourceLoaderImpl(dataLoader)));
 
-				// or the resource file itself
-				// resourcesMonitor.monitor(new ResourceMonitor(resourceManager.get(id), FileMonitorFactory.classPathFileMonitor(file)));
-				fileMonitorResourceHelper.monitorClassPathFile(file, resourceManager.get(id));
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("failed to load image mapping", e);
+			// // mark the resource for reloading whenever the properties file was modified
+			// resourcesMonitor.monitor(new ResourceMonitor(resourceManager.get(id), FileMonitorFactory.classPathFileMonitor(propertiesFile)));
+
+			// or the resource file itself
+			// resourcesMonitor.monitor(new ResourceMonitor(resourceManager.get(id), FileMonitorFactory.classPathFileMonitor(file)));
+			fileMonitorResourceHelper.monitorClassPathFile(file, resourceManager.get(id));
 		}
 	}
 }
