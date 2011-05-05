8525f26009f46f0a0688a0e1053c4c4db21bb256
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-05 18:56:35 -0300

    added a bit of javadocs

--- resourcesmanager-core/src/main/java/com/gemserk/resources/ResourceManager.java
+++ resourcesmanager-core/src/main/java/com/gemserk/resources/ResourceManager.java
@@ -2,15 +2,36 @@ package com.gemserk.resources;
 
 import com.gemserk.resources.resourceloaders.ResourceLoader;
 
+/**
+ * Provides a common way to handle Resources creation and destruction.
+ * 
+ * @author acoppes
+ */
 public interface ResourceManager<K> {
 
 	@SuppressWarnings("rawtypes")
 	void add(K id, ResourceLoader resourceLoader);
 
+	/**
+	 * Returns a Resource identified by id.
+	 * 
+	 * @param id
+	 *            the Resource identifier.
+	 */
 	<T> Resource<T> get(K id);
-	
+
+	/**
+	 * Returns the value of an existent Resource. If the resource doesn't exists it returns null.
+	 * 
+	 * @param id
+	 *            the Resource identifier.
+	 * @return the value of the Resource if it exists, null otherwise.
+	 */
 	<T> T getResourceValue(K id);
-	
+
+	/**
+	 * Unloads all created resources.
+	 */
 	void unloadAll();
 
 }
--- resourcesmanager-core/src/main/java/com/gemserk/resources/resourceloaders/ResourceLoader.java
+++ resourcesmanager-core/src/main/java/com/gemserk/resources/resourceloaders/ResourceLoader.java
@@ -2,8 +2,17 @@ package com.gemserk.resources.resourceloaders;
 
 import com.gemserk.resources.Resource;
 
+/**
+ * Defines a way to load a Resource.
+ * 
+ * @author acoppes
+ * 
+ */
 public interface ResourceLoader<T> {
 
+	/**
+	 * Loads and returns a Resource
+	 */
 	Resource<T> load();
-	
+
 }
