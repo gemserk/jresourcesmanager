4dd959724d8d5051607b5b46587dd117039a189a
Ariel Coppes <ariel.coppes@gemserk.com>
2011-12-09 14:12:19 -0200

    added javadocs to the ResoruceStatusMonitor class

--- resourcesmanager-core/src/main/java/com/gemserk/resources/monitor/ResourceStatusMonitor.java
+++ resourcesmanager-core/src/main/java/com/gemserk/resources/monitor/ResourceStatusMonitor.java
@@ -2,12 +2,17 @@ package com.gemserk.resources.monitor;
 
 import com.gemserk.resources.Resource;
 
+/**
+ * Provides a way to monitor the status changes (load and unload) of a Resource to possible take actions, for example, call a ResourceStatusChangedHandler.
+ * 
+ * @author acoppes
+ */
 public class ResourceStatusMonitor {
-	
+
 	Resource<?> monitoredResource;
 	boolean loaded;
 	boolean unloaded;
-	
+
 	public void setMonitoredResource(Resource<?> monitoredResource) {
 		this.monitoredResource = monitoredResource;
 		loaded = monitoredResource.isLoaded();
@@ -17,15 +22,24 @@ public class ResourceStatusMonitor {
 	public ResourceStatusMonitor(Resource<?> monitoredResource) {
 		setMonitoredResource(monitoredResource);
 	}
-	
+
+	/**
+	 * Returns true whenever the Resource was loaded.
+	 */
 	public boolean wasLoaded() {
 		return loaded;
 	}
-	
+
+	/**
+	 * Returns true whenever the Resource was loaded.
+	 */
 	public boolean wasUnloaded() {
 		return unloaded;
 	}
-	
+
+	/**
+	 * Call to update the monitor by checking the Resource latest status and to know if it was loaded or unloaded.
+	 */
 	public void checkChanges() {
 		loaded = !loaded && monitoredResource.isLoaded();
 		unloaded = !unloaded && !monitoredResource.isLoaded();
