be68557ba0bde69491f8a692c6f71bfc9edc87e3
Ariel Coppes <ariel.coppes@gemserk.com>
2012-08-20 15:52:01 -0300

    removed slick module from the project, not used nor soported any more

--- resourcesmanager-tests/src/main/java/com/gemserk/resources/ResourcesBuilder.java
+++ resourcesmanager-tests/src/main/java/com/gemserk/resources/ResourcesBuilder.java
@@ -3,15 +3,12 @@ package com.gemserk.resources;
 import com.gemserk.resources.datasources.DataSourceProvider;
 import com.gemserk.resources.datasources.DataSourceProvider.Source;
 import com.gemserk.resources.java2d.Java2dResourcesBuilder;
-import com.gemserk.resources.slick.SlickResourcesBuilder;
 
 /**
  * Provides an easier way to add resources to ResourceManager for java2d application.
  */
 public class ResourcesBuilder {
 
-	protected SlickResourcesBuilder slick;
-	
 	protected Java2dResourcesBuilder java2d;
 	
 	@SuppressWarnings("rawtypes")
@@ -21,7 +18,6 @@ public class ResourcesBuilder {
 	
 	@SuppressWarnings("rawtypes")
 	public ResourcesBuilder(ResourceManager resourceManager, DataSourceProvider dataSourceProvider) {
-		this.slick = new SlickResourcesBuilder(resourceManager);
 		this.java2d = new Java2dResourcesBuilder(resourceManager, dataSourceProvider);
 	}
 
